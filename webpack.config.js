f7614222e943005b232d885cf87951034632c7f0
gcanti <giulio.canti@gmail.com>
2017-12-16 09:07:28 +0100

    upgrade fp-ts-routing

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,28 @@
+const webpack = require('webpack')
 const path = require('path')
+
+const plugins = [
+  // new webpack.DefinePlugin({
+  //   'process.env.NODE_ENV': JSON.stringify('production')
+  // }),
+  // new webpack.optimize.UglifyJsPlugin({
+  //   compress: {
+  //     warnings: false,
+  //     screw_ie8: true,
+  //     conditionals: true,
+  //     unused: true,
+  //     comparisons: true,
+  //     sequences: true,
+  //     dead_code: true,
+  //     evaluate: true,
+  //     if_return: true,
+  //     join_vars: true
+  //   },
+  //   output: {
+  //     comments: false
+  //   }
+  // })
+]
 
 module.exports = {
   entry: './src/index.ts',
@@ -9,6 +33,14 @@ module.exports = {
     extensions: ['.js', '.ts', '.tsx']
   },
   module: {
-    loaders: [{ test: /\.tsx?$|\.jsx?$/, loader: 'awesome-typescript-loader', exclude: /node_modules/ }]
-  }
+    loaders: [
+      {
+        test: /\.tsx?$|\.jsx?$/,
+        loader: 'awesome-typescript-loader',
+        exclude: /node_modules/,
+        options: { silent: true }
+      }
+    ]
+  },
+  plugins
 }
